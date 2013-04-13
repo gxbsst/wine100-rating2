c8e9cee7364355f0c0d084d641e940ae7f180853
gxbsst <gxbsst@gmail.com>
2013-04-13 18:59:59 +0800

    INIT PROJECT.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wine100::Application.config.secret_token = 'cabc3c5e283923a61d4122ca01817a7beb6b344c76bdda1d3abeb7e394b201523833268dc25d615f2ca9621bffff88bd00f0146ab070abbb5bc6c56fb4725dcb'
