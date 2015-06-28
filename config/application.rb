336780e459e4ffc241677bdc782113163b048d60
gxbsst <gxbsst@gmail.com>
2015-06-28 20:52:16 +0800

    最新的更新

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,6 @@ module Wine100
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    I18n.enforce_available_locales = false
   end
 end
