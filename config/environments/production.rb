c5fc0cc7b835108c2fb372e77df84fefd41a42e9
gxbsst <gxbsst@gmail.com>
2015-10-15 00:05:09 +0800

    没有显示css的问题。

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,7 @@ Wine100::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.serve_static_files = true
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
